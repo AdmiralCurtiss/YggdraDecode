93c3562cb9f39b8d60c1b38470477f5e2adc343f
Admiral H. Curtiss <pikachu025@gmail.com>
2023-09-17 20:07:57 +0200

    Add packing support. (#1)

--- YggdraDecode/main.cpp
+++ YggdraDecode/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <array>
 #include <cstdint>
 #include <cstdio>
@@ -9,6 +10,26 @@
 
 #include "md5.h"
 #include "zlib.h"
+
+bool case_insensitive_equals(char lhs, char rhs) {
+    const char c0 = (lhs >= 'a' && lhs <= 'z') ? (lhs - ('a' + 'A')) : lhs;
+    const char c1 = (rhs >= 'a' && rhs <= 'z') ? (rhs - ('a' + 'A')) : rhs;
+    return c0 == c1;
+}
+
+bool ends_with_case_insensitive(std::string_view string, std::string_view ending) {
+    if (string.size() < ending.size()) {
+        return true;
+    }
+    for (size_t i = 0; i < ending.size(); ++i) {
+        const char cs = string[string.size() - ending.size() + i];
+        const char ce = ending[i];
+        if (!case_insensitive_equals(cs, ce)) {
+            return false;
+        }
+    }
+    return true;
+}
 
 std::string rot13(std::string_view input) {
     std::string s;
@@ -33,7 +54,7 @@ std::string rot13(std::string_view input) {
     return s;
 }
 
-void decrypt(char* dst, char* src, size_t length, std::string_view filename) {
+void Crypt(char* dst, char* src, size_t length, std::string_view filename) {
     if ((length % 4) != 0) {
         throw "length must be divisible by 4";
     }
@@ -66,9 +87,22 @@ std::vector<char> ReadDecrypted(FILE* f, size_t offset, size_t length, std::stri
     std::vector<char> out_data;
     out_data.resize(in_data.size());
 
-    decrypt(out_data.data(), in_data.data(), in_data.size(), filename);
+    Crypt(out_data.data(), in_data.data(), in_data.size(), filename);
 
     return out_data;
+}
+
+std::vector<char> Encrypt(const std::vector<char>& in_data, std::string_view filename) {
+    std::vector<char> input = in_data;
+    while ((input.size() % 4) != 0) {
+        input.push_back(0);
+    }
+    std::vector<char> output;
+    output.resize(input.size());
+
+    Crypt(output.data(), input.data(), input.size(), filename);
+
+    return output;
 }
 
 std::vector<char> Decompress(const std::vector<char>& out_data) {
@@ -86,7 +120,37 @@ std::vector<char> Decompress(const std::vector<char>& out_data) {
     zs.next_out = (Bytef*)decomp_data.data();
     inflate(&zs, Z_FINISH);
 
+    inflateEnd(&zs);
+
     return decomp_data;
+}
+
+std::vector<char> Compress(const std::vector<char>& in_data) {
+    std::vector<char> comp_data;
+    uint32_t decompSize = static_cast<uint32_t>(in_data.size());
+    if (in_data.size() != static_cast<size_t>(decompSize)) {
+        throw "data too long to compress";
+    }
+
+    z_stream zs{};
+    deflateInit(&zs, 9);
+
+    auto bound = deflateBound(&zs, decompSize);
+    comp_data.resize(static_cast<size_t>(bound) + 4);
+    std::memcpy(comp_data.data(), &decompSize, 4);
+
+    zs.avail_in = decompSize;
+    zs.next_in = (Bytef*)in_data.data();
+    zs.avail_out = bound;
+    zs.next_out = (Bytef*)comp_data.data() + 4;
+    deflate(&zs, Z_FINISH);
+    auto avail_out = zs.avail_out;
+    deflateEnd(&zs);
+
+    comp_data.resize((static_cast<size_t>(bound) - avail_out) + 4);
+
+
+    return comp_data;
 }
 
 struct FileTableEntry {
@@ -117,11 +181,14 @@ void Extract(FILE* f, std::string outfolder, std::vector<FileTableEntry>& fileTa
     std::string outpath = outfolder + "/" + e.Name;
 
     if (isFolder) {
+        // printf("Extracting folder: Length: %zu, Name: %s\n", size, e.Name.c_str());
         size_t folder_offset = e.DataOffset / 12;
         for (size_t i = 0; i < size; ++i) {
             Extract(f, outpath, fileTable, folder_offset + i, data_offset);
         }
     } else {
+        // printf("Extracting file: Length: %zu, Name: %s, Compressed: %s\n", size, e.Name.c_str(),
+        //        isCompressed ? "yes" : "no");
         size_t extra_bytes = size & 3;
         size_t aligned_size = extra_bytes ? (size + 4 - extra_bytes) : size;
 
@@ -136,28 +203,13 @@ void Extract(FILE* f, std::string outfolder, std::vector<FileTableEntry>& fileTa
     }
 }
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        printf("usage: YggdraDecode data.bin\n");
-        return -1;
-    }
-
-    std::string infilepath = argv[1];
-
-    // const char* filename = "NowLoading16.webp";
-    // size_t filesize = 0x34c8;
-    // size_t offset = 0x4d4c;
-
-    // const char* filename = "Card_TacSele.pck";
-    // size_t filesize = 0x000402a5 * 4;
-    // size_t offset = 0xa19e298;
-
+int ExtractArchive(FILE* f, const std::string& outfilepath) {
     const char* filename = "InfoData";
-    size_t infodata_filesize = 0x4d44;
-    size_t infodata_offset = 0x8;
+    uint32_t infodata_filesize = 0;
+    const size_t infodata_offset = 0x8;
     std::array<char, 8> infodata_info_bytes;
 
-    FILE* f = fopen(infilepath.c_str(), "rb");
+    _fseeki64(f, 0, SEEK_SET);
     fread(infodata_info_bytes.data(), 1, 8, f);
 
     std::memcpy(&infodata_filesize, infodata_info_bytes.data(), 4);
@@ -170,9 +222,15 @@ int main(int argc, char** argv) {
     std::vector<char> out_data;
     out_data.resize(in_data.size());
 
-    decrypt(out_data.data(), in_data.data(), in_data.size(), filename);
+    Crypt(out_data.data(), in_data.data(), in_data.size(), filename);
 
     std::vector<char> decomp_data = Decompress(out_data);
+
+    //{
+    //    FILE* f3 = fopen((outfilepath + "_InfoData").c_str(), "wb");
+    //    fwrite(decomp_data.data(), 1, decomp_data.size(), f3);
+    //    fclose(f3);
+    //}
 
     std::vector<FileTableEntry> fileTable;
     {
@@ -210,6 +268,253 @@ int main(int argc, char** argv) {
     }
 
     for (size_t i = 0; i < fileTable.size(); ++i) {
-        Extract(f, infilepath + ".ex", fileTable, i, infodata_offset + infodata_filesize);
+        Extract(f, outfilepath, fileTable, i, infodata_offset + infodata_filesize);
     }
+
+    return 0;
+}
+
+struct PackFileEntryInternal {
+    std::filesystem::path Path;
+    std::string Name;
+    bool IsFolder = false;
+    std::vector<PackFileEntryInternal> Children;
+};
+
+struct PackFileEntry {
+    std::filesystem::path Path;
+    uint64_t Length = 0;
+    uint64_t Offset = 0;
+    std::string Name;
+    bool IsFolder = false;
+
+    bool IsRead = false;
+    bool IsCompressed = false;
+    bool IsEncrypted = false;
+    std::vector<char> Data;
+};
+
+void CollectPackFileEntriesInternal(std::vector<PackFileEntryInternal>& entries,
+                                    const std::filesystem::path& p) {
+    for (const auto& entry : std::filesystem::directory_iterator(p)) {
+        if (entry.is_regular_file()) {
+            entries.emplace_back(
+                PackFileEntryInternal{entry.path(), entry.path().filename().string(), false});
+        } else if (entry.is_directory()) {
+            size_t index = entries.size();
+            auto& d = entries.emplace_back(
+                PackFileEntryInternal{entry.path(), entry.path().filename().string(), true});
+
+            CollectPackFileEntriesInternal(d.Children, entry.path());
+
+            std::stable_sort(d.Children.begin(), d.Children.end(),
+                             [](const PackFileEntryInternal& lhs,
+                                const PackFileEntryInternal& rhs) { return lhs.Name > rhs.Name; });
+        }
+    }
+}
+
+void FlattenPackFileEntries(std::vector<PackFileEntry>& flat,
+                            const std::vector<PackFileEntryInternal>& entries) {
+    size_t startIndex = flat.size();
+    for (const auto& e : entries) {
+        auto& f = flat.emplace_back();
+        f.Path = e.Path;
+        f.Name = e.Name;
+        f.IsFolder = e.IsFolder;
+    }
+    for (size_t i = 0; i < entries.size(); ++i) {
+        const auto& e = entries[i];
+        auto& f = flat[startIndex + i];
+        if (e.IsFolder) {
+            f.Length = e.Children.size();
+            f.Offset = flat.size();
+            FlattenPackFileEntries(flat, e.Children);
+        }
+    }
+}
+
+std::vector<PackFileEntry> CollectPackFileEntries(const std::filesystem::path& p) {
+    std::vector<PackFileEntryInternal> entries;
+    CollectPackFileEntriesInternal(entries, p);
+    std::vector<PackFileEntry> flat;
+    FlattenPackFileEntries(flat, entries);
+    return flat;
+}
+
+int PackArchive(const std::string& infilepath, const std::string& outfilepath) {
+    FILE* f = fopen(outfilepath.c_str(), "wb");
+    if (!f) {
+        return -1;
+    }
+
+    std::vector<PackFileEntry> entries = CollectPackFileEntries(std::filesystem::path(infilepath));
+
+    uint64_t totalLength = 0;
+    for (auto& entry : entries) {
+        if (!entry.IsFolder) {
+            bool shouldCompress = !(ends_with_case_insensitive(entry.Name, ".pck")
+                                    || ends_with_case_insensitive(entry.Name, ".webp")
+                                    || ends_with_case_insensitive(entry.Name, ".webm")
+                                    || ends_with_case_insensitive(entry.Name, ".png")
+                                    || ends_with_case_insensitive(entry.Name, ".ogg")
+                                    || ends_with_case_insensitive(entry.Name, ".opus"));
+
+            FILE* f2 = fopen(entry.Path.string().c_str(), "rb");
+            _fseeki64(f2, 0, SEEK_END);
+            auto length = _ftelli64(f2);
+            _fseeki64(f2, 0, SEEK_SET);
+            entry.Data.resize(length);
+            fread(entry.Data.data(), 1, length, f2);
+            fclose(f2);
+
+            entry.IsRead = true;
+            if (shouldCompress) {
+                auto compressed = Compress(entry.Data);
+                if (compressed.size() < entry.Data.size()) {
+                    entry.Data = std::move(compressed);
+                    entry.IsCompressed = true;
+                }
+            }
+
+            entry.Length = entry.Data.size();
+            auto encrypted = Encrypt(entry.Data, entry.Name);
+            entry.Data = std::move(encrypted);
+            entry.IsEncrypted = true;
+
+            uint64_t extraBytes = entry.Length & 3;
+            uint64_t alignedLength = extraBytes ? (entry.Length + 4 - extraBytes) : entry.Length;
+            entry.Offset = totalLength;
+            totalLength += alignedLength;
+        }
+    }
+
+    struct HeaderEntry {
+        uint32_t NameOffset; // offset into the strings section of InfoData
+        uint32_t Length;     // two highest bits are flags
+        uint32_t DataOffset; // offset into the data.bin
+    };
+    std::vector<HeaderEntry> headerData;
+    std::vector<char> headerStrings;
+    for (size_t i = 0; i < entries.size(); ++i) {
+        const auto& entry = entries[i];
+
+        const auto write_string = [&headerStrings](std::string_view sv) -> size_t {
+            size_t pos = headerStrings.size();
+            for (char c : sv) {
+                headerStrings.push_back(c);
+            }
+            headerStrings.push_back(0);
+            return pos;
+        };
+
+        size_t nameOffset = write_string(entry.Name);
+        uint32_t nameOffset32 = static_cast<uint32_t>(nameOffset);
+        if (nameOffset != static_cast<size_t>(nameOffset32)) {
+            throw "string table too big";
+        }
+        uint32_t length = 0;
+        uint32_t dataOffset = 0;
+        if (entry.IsFolder) {
+            if (entry.Length > 0x3fff'ffffu) {
+                throw "too many files in folder";
+            }
+
+            length = static_cast<uint32_t>(entry.Length | 0x8000'0000u);
+            uint64_t dataOffset64 = entry.Offset * 12;
+            dataOffset = static_cast<uint32_t>(dataOffset64);
+            if (dataOffset != static_cast<uint64_t>(dataOffset64)) {
+                throw "file table too big";
+            }
+        } else {
+            if (entry.Length > 0x3fff'ffffu) {
+                throw "single file too big";
+            }
+
+            length = static_cast<uint32_t>(entry.Length);
+            if (entry.IsCompressed) {
+                length = length | 0x4000'0000u;
+            }
+            uint64_t dataOffset64 = entry.Offset;
+            dataOffset = static_cast<uint32_t>(dataOffset64);
+            if (dataOffset != static_cast<uint64_t>(dataOffset64)) {
+                throw "combined files too big";
+            }
+        }
+
+        headerData.emplace_back(HeaderEntry{nameOffset32, length, dataOffset});
+    }
+
+    std::vector<char> infodata;
+    size_t infodataLength = 8 + headerData.size() * 12 + headerStrings.size();
+    infodata.resize(infodataLength);
+    uint32_t headerDataLength = headerData.size() * 12;
+    uint32_t headerStringsLength = headerStrings.size();
+    std::memcpy(infodata.data(), &headerDataLength, 4);
+    std::memcpy(infodata.data() + 4, &headerStringsLength, 4);
+    for (size_t i = 0; i < headerData.size(); ++i) {
+        std::memcpy(infodata.data() + 8 + i * 12, &headerData[i].NameOffset, 4);
+        std::memcpy(infodata.data() + 8 + i * 12 + 4, &headerData[i].Length, 4);
+        std::memcpy(infodata.data() + 8 + i * 12 + 8, &headerData[i].DataOffset, 4);
+    }
+    std::memcpy(infodata.data() + 8 + headerData.size() * 12, headerStrings.data(),
+                headerStrings.size());
+
+    auto infodataCompressed = Compress(infodata);
+    size_t infodataCompressedLength = infodataCompressed.size();
+    size_t infodataExtraBytes = infodataCompressedLength & 3;
+    size_t infodataAlignedLength = infodataExtraBytes
+                                       ? (infodataCompressedLength + 4 - infodataExtraBytes)
+                                       : infodataCompressedLength;
+    infodataCompressed.resize(infodataAlignedLength);
+    std::vector<char> infodataEncrypted;
+    infodataEncrypted.resize(infodataCompressed.size());
+    Crypt(infodataEncrypted.data(), infodataCompressed.data(), infodataCompressed.size(),
+          "InfoData");
+
+
+    // header
+    std::array<char, 8> infodata_info_bytes{};
+    uint32_t infodata_filesize = infodataCompressedLength;
+    uint32_t content_filesize = totalLength;
+    std::memcpy(infodata_info_bytes.data(), &infodata_filesize, 4);
+    std::memcpy(infodata_info_bytes.data() + 4, &content_filesize, 4);
+    fwrite(infodata_info_bytes.data(), infodata_info_bytes.size(), 1, f);
+
+    // infodata
+    fwrite(infodataEncrypted.data(), infodataEncrypted.size(), 1, f);
+
+    // files
+    for (auto& entry : entries) {
+        if (!entry.IsFolder) {
+            fwrite(entry.Data.data(), entry.Data.size(), 1, f);
+        }
+    }
+
+    fclose(f);
+
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("Usage for unpacking: YggdraDecode file.bin\n");
+        printf("Usage for packing: YggdraDecode folder\n");
+        return -1;
+    }
+
+    std::string infilepath(argv[1]);
+    while (infilepath.size() > 0 && (infilepath.back() == '/' || infilepath.back() == '\\')) {
+        infilepath.pop_back();
+    }
+    FILE* f = fopen(infilepath.c_str(), "rb");
+    if (f) {
+        int rv = ExtractArchive(f, infilepath + ".ex");
+        fclose(f);
+        return rv;
+    } else if (std::filesystem::is_directory(std::filesystem::path(infilepath))) {
+        return PackArchive(infilepath, infilepath + "_new.bin");
+    }
+
+    return -1;
 }
